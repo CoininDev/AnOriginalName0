e234aae77ad9396a5b0d660635d1145e42c34680
Gabriel DB <119068235+CoininDev@users.noreply.github.com>
2025-09-13 06:31:07 +0000

    feat: improve the endpoints

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -9,15 +9,15 @@ import (
 func SetupRoutes(r *gin.Engine, db *gorm.DB) {
 	otc := &controller.OgTextController{DB: db}
 
-	/// Original just post the text to the db
-	r.POST("/original", otc.CreateOgText)
+	/// It just post the text to the db
+	r.POST("/texts", otc.CreateOgText)
 
 	/// Compare get the text from the db and compare it to the input text
 	/// This does not store anything in the db
-	r.GET("/compare", otc.CompareText)
+	r.POST("/texts/compare", otc.CompareText)
 
 	/// Compare get the text from the db and compare it to the input text
 	/// This stores the comparison result in the db
 	/// Returns identically structured response as GET /compare
-	r.POST("/compare", otc.CompareFeed)
+	r.POST("/texts/compare-and-feed", otc.CompareFeed)
 }
