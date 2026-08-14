28ea95112f4e68425995cd82992319c9637f2d2c
CoininDev <gabr.batz@gmail.com>
2026-08-14 14:00:22 -0300

    feat: configure FastAPI with CORS and add Docker support

--- server/embeddings_microservice/api.py
+++ server/embeddings_microservice/api.py
@@ -1,11 +1,14 @@
-from sentence_transformers import SentenceTransformer
-from fastapi.middleware.cors import CORSMiddleware
+import os
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from sentence_transformers import SentenceTransformer
 
 app = FastAPI()
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-origins = ["*"]
+origins = [
+    os.getenv("BACKEND_URL", "http://localhost:6868"),
+]
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,10 +18,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 @app.post("/embed")
 async def embed_text(request: Request):
     txt = await request.body()
     data = txt.decode("utf-8")
     embedding = model.encode(data).tolist()
     return {"embedding": embedding, "length": len(embedding)}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 6969))
+    uvicorn.run(app, host="0.0.0.0", port=port)
